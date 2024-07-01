17d605a12802c9e48fa022374f118eb4a9ca7983
mequqqe <dauletturlykoza33@gmail.com>
2024-07-01 15:23:45 +0500

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,43 @@
+import { setupLayouts } from 'virtual:generated-layouts'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from '~pages'
+import { isUserLoggedIn } from './utils'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: { name: 'dashboards-analytics' },
+    },
+    {
+      path: '/pages/user-profile',
+      redirect: () => ({ name: 'pages-user-profile-tab', params: { tab: 'profile' } }),
+    },
+    {
+      path: '/pages/account-settings',
+      redirect: () => ({ name: 'pages-account-settings-tab', params: { tab: 'account' } }),
+    },
+    ...setupLayouts(routes),
+  ],
+})
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = isUserLoggedIn()
+
+  if (isLoggedIn) {
+    if (to.meta.redirectIfLoggedIn) {
+      next({ name: 'dashboards-analytics' })
+    } else {
+      next()
+    }
+  } else {
+    if (to.name !== 'login') {
+      next({ name: 'login', query: { to: to.fullPath !== '/' ? to.fullPath : undefined } })
+    } else {
+      next()
+    }
+  }
+})
+
+export default router
